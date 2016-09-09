f49a5e0ea3996e3db547b6a0a95fc86d0aca817d
Damian Baćkowski <damianbackowski@gmail.com>
2016-09-09 16:36:00 +0200

    fix bugs

--- double_scroll.js
+++ double_scroll.js
@@ -1,27 +1,40 @@
 (function($) {
   'use strict';
 
+  var scrollWrapper = function() {
+    var element = $(this);
+    var elementWidth = element.width();
+    var elementScrollWidth = element[0] ? element[0].scrollWidth : elementWidth;
+
+    if (elementWidth != elementScrollWidth) {
+      var scrollWrapperTemplate = '<div id="double-scroll" class="scroll-wrapper" style="width: ' + elementWidth + 'px; overflow-x: scroll; overflow-y: hidden;">';
+      scrollWrapperTemplate += '<div class="scroll-top" style="width: ' + elementScrollWidth + 'px; height: 1px"></div>';
+      scrollWrapperTemplate += '</div>';
+
+      if ($('#double-scroll').length) {
+        $('#double-scroll').replaceWith(scrollWrapperTemplate);
+      } else {
+        element.before(scrollWrapperTemplate);
+      }
+
+      $(".scroll-wrapper").scroll(function() {
+        element.scrollLeft($(".scroll-wrapper").scrollLeft());
+      });
+
+      element.scroll(function() {
+        $(".scroll-wrapper").scrollLeft(element.scrollLeft());
+      });
+    } else {
+      if ($('#double-scroll').length) {
+        $('#double-scroll').hide();
+      }
+    }
+  }
+
   var methods = {
     initialize: function(options) {
-      var element = $(this);
-      var elementWidth = element.width();
-      var elementScrollWidth = element[0] ? element[0].scrollWidth : elementWidth;
-
-      if (elementWidth != elementScrollWidth) {
-        var wrapperTemplate = '<div class="scroll-wrapper" style="width: ' + elementWidth + 'px; overflow-x: scroll; overflow-y: hidden;">';
-        wrapperTemplate += '<div class="scroll-top" style="width: ' + elementScrollWidth + 'px; height: 1px"></div>';
-        wrapperTemplate += '</div>';
-
-        element.before(wrapperTemplate);
-
-        $(".scroll-wrapper").scroll(function(){
-          element.scrollLeft($(".scroll-wrapper").scrollLeft());
-        });
-
-        element.scroll(function(){
-          $(".scroll-wrapper").scrollLeft(element.scrollLeft());
-        });
-      }
+      scrollWrapper.call(this);
+      $(window).on('resize', scrollWrapper.bind(this));
     },
 
     destroy: function() {
